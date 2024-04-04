eb6c4694be5a92bfc4d272342848c9002104e6d5
durgapavanipallam <166044853+durgapavanipallam@users.noreply.github.com>
2024-04-04 21:51:35 +0530

    Add files via upload

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 import java.util.Random;
-
 public class NumberGuessingGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -11,15 +10,12 @@ public class NumberGuessingGame {
         int maxAttempts = 10
         int attempts = 0;
         boolean hasGuessedCorrectly = false;
-
         System.out.println("Welcome to the Number Guessing Game!");
         System.out.println("I have chosen a number between " + lowerBound + " and " + upperBound + ". Try to guess it!");
-
         while (attempts < maxAttempts && !hasGuessedCorrectly) {
             System.out.print("Enter your guess: ");
             int userGuess = scanner.nextInt();
             attempts++;
-
             if (userGuess == targetNumber) {
                 hasGuessedCorrectly = true;
                 System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts.");
@@ -28,11 +24,9 @@ public class NumberGuessingGame {
                 System.out.println("Incorrect guess. " + hint);
             }
         }
-
         if (!hasGuessedCorrectly) {
             System.out.println("Sorry, you've run out of attempts. The correct number was: " + targetNumber);
         }
-
         scanner.close();
     }
 }
